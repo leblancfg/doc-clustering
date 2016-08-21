92e5ed6dd820884f8d3aa6373a5605497cc188f1
leblancfg <leblancfg>
2016-08-20 21:30:01 -0400

    Better architecture

---
+++ gensim-lda.py
@@ -0,0 +1,81 @@
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+# Author: leblancfg
+# 2016
+
+"""
+Document Clustering from Service Desk tickets
+"""
+
+from stop_words import get_stop_words
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+import csv
+import os
+import logging
+import gensim
+
+def get_files(path):
+    # Extract ticket details from SVD database extract
+    file = open(path)
+    documents = list(csv.reader(file))
+
+    def column(matrix, i):
+        return [row[i] for row in matrix]
+
+    refnum = column(documents, ref_idx)
+    documents = column(documents, doc_idx)
+    return ([refnum, documents])
+
+def clustering(documents):
+    # Set up word handling
+    tokenizer = RegexpTokenizer(r'\w+')
+    en_stop = get_stop_words('en')
+    p_stemmer = PorterStemmer()
+
+    # Set up word handling
+    tokenizer = RegexpTokenizer(r'\w+')
+    en_stop = get_stop_words('en')
+    p_stemmer = PorterStemmer()
+
+    # See logging events
+    logger = logging.getLogger('gensim.corpora.mmcorpus')
+
+    # Perform the clustering
+    for i in documents:
+        texts = []
+        raw = i.lower()
+        tokens = tokenizer.tokenize(raw)
+
+        # Remove stop words from tokens
+        stopped_tokens = [i for i in tokens if not i in en_stop]
+
+        # Stem tokens
+        stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
+        # Add tokens to list
+        texts.append(stemmed_tokens)
+
+    dictionary = corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+
+    ldamodel = gensim.models.ldamodel.LdaModel(
+        corpus,
+        num_topics=2,
+        id2word=dictionary,
+        passes=20)
+    return ldamodel
+
+if __name__ == "__main__":
+    # Set up environment-based variables
+    ref_idx = 3  # Input file column index for the reference number
+    doc_idx = 7  # Input file column index for the ticket description
+
+    tickets = get_files('inputs/')[1]
+    model = clustering(tickets)
+
+    m = open('model.txt', 'a')
+    m.write(model)
+    m.close()
+
+
